87764ac078d3c7ccd0071895dec45b5a6d870b7a
Sakinaa782 <sashotrupa@hotmail.com>
2022-06-30 18:05:16 +0300

    Finished

--- main.js
+++ main.js
@@ -1,88 +1,159 @@
 //Operations
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const multiplication = document.getElementById('multiply');
-const division = document.getElementById('division');
-const equal = document.getElementById('equal');
-const ac = document.getElementById('AC');
-const dot = document.getElementById('dot')
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const multiplication = document.getElementById("multiply");
+const division = document.getElementById("division");
+const equal = document.getElementById("equal");
+const ac = document.getElementById("AC");
+const dot = document.getElementById("dot");
 //Numbers
-const one = document.getElementById('1');
-const two = document.getElementById('2');
-const three = document.getElementById('3');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six   = document.getElementById('6');
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine = document.getElementById('9');
-const zero = document.getElementById('0');
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const zero = document.getElementById("0");
 //Display
-const display = document.getElementById('textarea');
+const display = document.getElementById("textarea");
 
-// Variables
-
-let operation = '';
+// Global Variables
+let operation = "";
 let number = 0;
+let num2 = 0;
 let finalSum = 0;
 let addDot = "";
+let result = 0;
+let beforeSum = 0;
 //Operation Event Listeners
-plus.addEventListener('click', () =>{
-    operation = "+"
-})
-minus.addEventListener('click', () =>{
-   operation = "-"
-})
-multiplication.addEventListener('click', () =>{
-    operation = "*"
-})
-division.addEventListener('click', () =>{
-    operation = "/"
-})
-equal.addEventListener('click', () =>{
-    sumNums();
-})
-ac.addEventListener('click', () =>{
-    display.innerHTML = '';
-})
-dot.addEventListener('click', () =>{
-    addDot = "."
-})
+plus.addEventListener("click", () => {
+  operation = "+";
+});
+minus.addEventListener("click", () => {
+  operation = "-";
+});
+multiplication.addEventListener("click", () => {
+  operation = "*";
+});
+division.addEventListener("click", () => {
+  operation = "/";
+});
+equal.addEventListener("click", () => {
+  sumNums();
+});
+ac.addEventListener("click", () => {
+  display.innerHTML = "";
+  operation = "";
+  num2 = 0;
+  number = 0;
+});
+dot.addEventListener("click", () => {
+  addDot = ".";
+});
 //Number Event Listeners
 
-one.addEventListener('click', () =>{
-   number = 1;
-})
-two.addEventListener('click', () =>{
-    number = 2;
-})
-three.addEventListener('click', () =>{
-    number = 3;
-})
-four.addEventListener('click', () =>{
-    number = 4;
-})
-five.addEventListener('click', () =>{
-    number = 5;
-})
-six.addEventListener('click', () =>{
-    number = 6;
-})
-seven.addEventListener('click', () =>{
-    number = 7;
-})
-eight.addEventListener('click', () =>{
-    number = 8;
-})
-nine.addEventListener('click', () =>{
-    number = 9;
-})
-zero.addEventListener('click', () =>{
-    number = 0;
-})
-function sumNums(){
-    switch(operation){
-        case "+":
-            break; 
+one.addEventListener("click", () => {
+    if(operation !== "+"){
+        number = 1;
+    }else{
+        num2 = 1;
+       
+}
+  });
+  two.addEventListener("click", () => {
+    if(operation == ""){
+        number = 2;
+    }else {
+        num2 = 2;
+       
     }
+  });
+  three.addEventListener("click", () => {
+    if(operation == ""){
+      number = 3;
+  }else {
+      num2 = 3;
+     
+  }
+  });
+  four.addEventListener("click", () => {
+    if(operation == ""){
+      number = 4;
+  }else {
+      num2 = 4;
+     
+  }
+  });
+  five.addEventListener("click", () => {
+    if(operation == ""){
+      number = 5;
+  }else {
+      num2 = 5;
+     
+  }
+  });
+  six.addEventListener("click", () => {
+    if(operation == ""){
+      number = 6;
+  }else {
+      num2 = 6;
+     
+  }
+  });
+  seven.addEventListener("click", () => {
+    if(operation == ""){
+      number = 7;
+  }else {
+      num2 = 7;
+     
+  }
+  });
+  eight.addEventListener("click", () => {
+    if(operation == ""){
+      number = 8;
+  }else {
+      num2 = 8;
+     
+  }
+  });
+  nine.addEventListener("click", () => {
+    if(operation == ""){
+      number = 9;
+  }else {
+      num2 = 9;
+     
+  }
+  });
+  zero.addEventListener("click", () => {
+    if(operation == ""){
+      number = 0;
+  }else {
+      num2 = 0;
+     
+  }
+  });
+
+function sumNums() {
+  switch (operation) {
+    case "+":
+      result = num2 + number;
+      display.innerHTML = result;
+      break;
+    case "*":
+      result = num2 * number;
+      display.innerHTML = result;
+      break;
+    case "/":
+      result = num2 / number;
+      display.innerHTML = result;
+      break;
+    case "-":
+      result = num2 - number;
+      display.innerHTML = result;
+      break;
+    default: display.innerHTML = "Error"
+  }
 }
