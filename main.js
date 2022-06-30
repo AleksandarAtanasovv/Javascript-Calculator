9a1961d9fef22e8c454535b070d8f4abe4f0a1b5
Sakinaa782 <sashotrupa@hotmail.com>
2022-06-30 15:57:04 +0300

    Changes

---
+++ main.js
@@ -0,0 +1,32 @@
+//Operations
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const multiplication = document.getElementById('multiply');
+const division = document.getElementById('division');
+//Numbers
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six   = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const zero = document.getElementById('0');
+
+
+//Operation Event Listeners
+plus.addEventListener('click', () =>{
+    console.log(1);
+})
+minus.addEventListener('click', () =>{
+    console.log(1);
+})
+multiplication.addEventListener('click', () =>{
+    console.log(1);
+})
+division.addEventListener('click', () =>{
+    console.log(1);
+})
+//Number Event Listeners
